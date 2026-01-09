4be8682c2e3f8390ddfbe2bc3c8f3dc096e7b3fc
Gia Bao <nngogbao@example.com>
2026-01-09 23:06:19 +0700

    Update model submissions.

--- backend/apps/submissions/models.py
+++ backend/apps/submissions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# Gia Bảo
+class Submission(models.Model):
+    STATUS_CHOICES = [
+        ("submitted", "Submitted"),
+        ("under_review", "Under Review"),
+        ("accepted", "Accepted"),
+        ("rejected", "Rejected"),
+        ("camera_ready", "Camera Ready"),
+    ]
+    conference = models.ForeignKey(
+        "conferences.Conference",
+        on_delete=models.CASCADE
+    )#
