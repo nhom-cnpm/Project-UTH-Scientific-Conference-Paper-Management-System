f2381ab479fc77d6f0df1b2e48353810fc78a07e
baonhu2903 <nhuphb7510@ut.edu.vn>
2026-01-11 22:04:30 +0700

    update conference & review

--- backend/apps/conferences/models.py
+++ backend/apps/conferences/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class conferences(models.Model):
+    title = models.charField(max_length=200)
+    content = models.textField()
+    created_at = models.dateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
