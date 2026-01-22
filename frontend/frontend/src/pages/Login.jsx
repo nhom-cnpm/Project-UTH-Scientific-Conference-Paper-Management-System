f6116f7639d870e279a69f13f662828b76d4d16c
ntrang06 <trangbn9517@ut.edu.vn>
2026-01-22 10:25:51 +0700

    update Login.jsx,Login.css

--- frontend/frontend/src/pages/Login.jsx
+++ frontend/frontend/src/pages/Login.jsx
@@ -19,8 +19,6 @@ export default function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // ===== MOCK AUTHENTICATION =====
     if (form.username === "reviewer") {
       navigate("/reviewer");
     } else if (form.username === "chair") {
@@ -32,6 +30,9 @@ export default function Login() {
 
   return (
     <div className="login-container">
+      {/* 1. THÊM DÒNG NÀY Ở ĐÂY */}
+      <div className="header-bar">UTH - COMFMS</div>
+
       <div className="login-overlay">
         <div className="login-box">
           <h2>LOG IN TO THE SYSTEM</h2>
