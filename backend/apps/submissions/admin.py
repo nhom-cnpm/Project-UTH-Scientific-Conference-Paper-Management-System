799b1497641b5d2515dc7397b5aaef02cc28765f
Gia Bao <nngogbao@example.com>
2026-02-06 07:56:25 +0700

    Update structure file

--- backend/apps/submissions/admin.py
+++ backend/apps/submissions/admin.py
@@ -1,9 +1,2 @@
 from django.contrib import admin
-from .models import Submission, Topic
-
 # Register your models here.
-@admin.register(Submission)
-class SubmissionAdmin(admin.ModelAdmin):
-    list_display = ("id", "title",  "topic", "status", "paper_id", "camera_ready_deadline")
-    list_filter = ("status",)
-    search_fields = ("title", "paper_id")
--- backend/apps/submissions/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Submission
-
-class SubmissionSerializer(serializers.ModelSerializer):
-    # topic = serializers.CharField(source="topic.name", read_only = True)
-    class Meta:
-        model = Submission
-        fields = ["id", "title", "topic", "status"]
